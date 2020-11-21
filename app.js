e9e09ca6db74e23d7188cf2f06f1255f7f3e5ff4
Sarthak Arora <sarthakarora1503@gmail.com>
2020-11-22 00:41:46 +0530

    whole repo updated

--- app.js
+++ app.js
@@ -26,6 +26,20 @@ let transferSchema = new mongoose.Schema({
 let Customers = mongoose.model("Customers",customerSchema);
 let Transfers = mongoose.model("Transfers",transferSchema);
 
+Customers.deleteMany({},(err,record)=> {
+    if(err)
+        console.log(err);
+    else    
+        console.log(record);
+})
+
+Transfers.deleteMany({},(err,record)=> {
+    if(err)
+        console.log(err);
+    else    
+        console.log(record);
+})
+
 app.get("/",(req,res)=> {
     res.render("home");
 });
