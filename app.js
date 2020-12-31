59907e60379373b521676467727f585dfbb9b4a5
Sarthak Arora <sarthakarora1503@gmail.com>
2021-01-01 03:41:53 +0530

    icons removed, try hosting now

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
+// let mysql = require("mysql");
 let nodemon = require("nodemon");
-let http = require("http");
 let app = express();
 let dotenv = require("dotenv").config();
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true})); 
 
-let dbUrl = process.env.DB_URL || "mongodb://localhost:27017/bbsdb";
-// const port = 80;
+let dbUrl = "mongodb://localhost:27017/bbsdb";
+const port = 80;
 
 mongoose.connect(dbUrl,{
     useNewUrlParser: true,
@@ -53,10 +53,9 @@ app.get("/viewall/:customerid",async(req,res)=> {
     try {
         let cid = req.params.customerid;
 
-    let record = await Customers.findOne({_id:cid});
-    let allcustomers = await Customers.find({});
-
-    res.render("customerpage",{record:record,customers:allcustomers});
+        let record = await Customers.findOne({_id:cid});
+        let allcustomers = await Customers.find({});
+        res.render("customerpage",{record:record,customers:allcustomers});
     }
     catch (err) {
         console.error(error);
@@ -83,12 +82,12 @@ app.get("/viewall/:customerid/transferto/:transfercid",async(req,res)=> {
     
     try {
         let cid = req.params.customerid;
-    let transfercid = req.params.transfercid;
-    let record = await Customers.findOne({_id:cid});
-    let transfercust = await Customers.findOne({_id:transfercid});
-    let customers = await Customers.find({});
+        let transfercid = req.params.transfercid;
+        let record = await Customers.findOne({_id:cid});
+        let transfercust = await Customers.findOne({_id:transfercid});
+        let customers = await Customers.find({});
 
-    res.render("transferpage",{transfercust:transfercust,record:record,customers:customers,s:s});
+        res.render("transferpage",{transfercust:transfercust,record:record,customers:customers,s:s});
     }
     catch (err) {
         console.error(error);
@@ -102,57 +101,57 @@ app.get("/viewall/:customerid/transferto/:transfercid",async(req,res)=> {
 app.post("/viewall/:customerid/transferto/:transfercid",async(req,res)=> {
     try {
         let amount = req.body.amount;
-    let transfercid = req.params.transfercid;
-    let cid = req.params.customerid;
+        let transfercid = req.params.transfercid;
+        let cid = req.params.customerid;
 
-    let transfertocust = await Customers.findOne({_id:transfercid});
-    let customer = await Customers.findOne({_id:cid});
+        let transfertocust = await Customers.findOne({_id:transfercid});
+        let customer = await Customers.findOne({_id:cid});
 
-    if(amount > customer.currentBalance)
-    {
-        let url = "/viewall/" + cid + "/transferto/" + transfercid; 
-    
-        console.log("Insufficient balance for money transfer!!");
-        s=1;
-        res.redirect(url);
-    }
-    else
-    {
-        let transfernetamount = parseInt(transfertocust.currentBalance) + parseInt(amount);
+        if(amount > customer.currentBalance)
+        {
+            let url = "/viewall/" + cid + "/transferto/" + transfercid; 
         
-        transfertocust.updateOne({$set : {currentBalance: transfernetamount}},(err,record)=> {
-            if(err)
-                console.log(err);
-            else
-                console.log("Updated!!");
-        });
+            console.log("Insufficient balance for money transfer!!");
+            s=1;
+            res.redirect(url);
+        }
+        else
+        {
+            let transfernetamount = parseInt(transfertocust.currentBalance) + parseInt(amount);
+            
+            transfertocust.updateOne({$set : {currentBalance: transfernetamount}},(err,record)=> {
+                if(err)
+                    console.log(err);
+                else
+                    console.log("Updated!!");
+            });
 
-        let customchange = parseInt(customer.currentBalance) - parseInt(amount);
-        console.log(typeof transfertocust.currentBalance, typeof customchange, amount);
-        customer.updateOne({$set : {currentBalance: customchange}},(err,record)=> {
-            if(err)
-                console.log(err);
-            else
-                console.log("Updated this also!!");
-        });
+            let customchange = parseInt(customer.currentBalance) - parseInt(amount);
+            console.log(typeof transfertocust.currentBalance, typeof customchange, amount);
+            customer.updateOne({$set : {currentBalance: customchange}},(err,record)=> {
+                if(err)
+                    console.log(err);
+                else
+                    console.log("Updated this also!!");
+            });
 
-        let transferrecords = {
-            transferredby: customer.name,
-            transferredto: transfertocust.name,
-            amount: parseInt(amount)
-        };
+            let transferrecords = {
+                transferredby: customer.name,
+                transferredto: transfertocust.name,
+                amount: parseInt(amount)
+            };
 
-        Transfers.insertMany(transferrecords,(err,record)=> {
-            if(err)
-                console.log(err);
-            else
-                console.log("Record stored!!");
-        });
-        console.log(Transfers);
+            Transfers.insertMany(transferrecords,(err,record)=> {
+                if(err)
+                    console.log(err);
+                else
+                    console.log("Record stored!!");
+            });
+            console.log(Transfers);
 
-        s=0;
-        res.redirect("/successfultransaction");
-    }
+            s=0;
+            res.redirect("/successfultransaction");
+        }
     }
     catch(error) {
         console.error(error);
@@ -175,6 +174,6 @@ app.get("/successfultransaction",(req,res)=> {
     res.render("success");
 });
 
-app.listen(process.env.PORT || 3000,process.env.IP, ()=> {
+app.listen(process.env.PORT || 80, ()=> {
         console.log("The server is listening!!");
     });
