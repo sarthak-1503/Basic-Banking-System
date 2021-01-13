096c477717579ab36d38aca2b499830131326f5e
Sarthak Arora <sarthakarora1503@gmail.com>
2021-01-13 17:15:54 +0530

    still trying to deploy

--- app.js
+++ app.js
@@ -48,23 +48,23 @@ mongoose.connect(dbUrl,{
 });
 
 let memCache = new cache.Cache();
-    let cacheMiddleware = (duration) => {
-        return (req, res, next) => {
-            let key =  '__express__' + req.originalUrl || req.url
-            let cacheContent = memCache.get(key);
-            if(cacheContent){
-                res.send( cacheContent );
-                return
-            }else{
-                res.sendResponse = res.send
-                res.send = (body) => {
-                    memCache.put(key,body,duration*1000);
-                    res.sendResponse(body)
-                }
-                next()
+let cacheMiddleware = (duration) => {
+    return (req, res, next) => {
+        let key =  '__express__' + req.originalUrl || req.url
+        let cacheContent = memCache.get(key);
+        if(cacheContent){
+            res.send( cacheContent );
+            return
+        }else{
+            res.sendResponse = res.send
+            res.send = (body) => {
+                memCache.put(key,body,duration*1000);
+                res.sendResponse(body)
             }
+            next()
         }
     }
+}
 
 let customerSchema = new mongoose.Schema({
     name: String,
